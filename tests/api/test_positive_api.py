6f565cde88bbd8956c28cd4bd854ea30db3c9cf0
GlebAQA <galievgleb99@gmail.com>
2023-08-20 19:04:55 +0300

    add class TestMainPage

--- tests/api/test_positive_api.py
+++ tests/api/test_positive_api.py
@@ -1,8 +1,24 @@
 import pytest
-from api.api_checks import check_positive_api_response
+import requests
 
 
-@pytest.mark.parametrize("page", [1, 2])
-def test_positive_api_users(api_client, page):
-    response = api_client.get_users(page)
-    check_positive_api_response(response)
+class TestMainPage:
+    def test_create(self):
+        data = {
+                "email": "eve.holt@reqres.in",
+                "password": "pistol"
+            }
+
+        response1 = requests.post("https://reqres.in/api/register", data=data)
+
+        assert "auth_sid" in response1.cookies, "There is no auth cookie in the response"
+        assert "x-csrf-token" in response1.headers, "There is no CSRF in the response"
+        assert "user_id" in response1.json, "There is no user id in the response"
+
+        auth_sid = response1.cookies.get("auth_sid")
+        token = response1.headers.get("x-csrf-token")
+        user_id_from_auth_method = response1.json()["user_id"]
+
+        # response2 = requests.get(
+        #     ""
+        # )
